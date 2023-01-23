86f06e956262953c46dd6df84784d0b880b2f0cd
Kritika Mittal <81286069+kritwit@users.noreply.github.com>
2023-01-23 15:52:58 +0530

    Update Selection__Sort.c

--- Selection__Sort.c
+++ Selection__Sort.c
@@ -1,8 +1,25 @@
-void selection_sort(int arr[], int n) {
-    int i, j, min_index, temp;
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter elements in array: ");
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    
+    printf("The original array: ");
+    for(int i=0;i<n;i++)
+        printf("%d ",arr[i]);
+    
+    int i, j, min_index, temp, comparisons = 0, swaps = 0;
     for (i = 0; i < n-1; i++) {
         min_index = i;
         for (j = i+1; j < n; j++) {
+            
+            comparisons++;
             if (arr[j] < arr[min_index]) {
                 min_index = j;
             }
@@ -10,5 +27,13 @@ void selection_sort(int arr[], int n) {
         temp = arr[min_index];
         arr[min_index] = arr[i];
         arr[i] = temp;
+        swaps++;
     }
+    
+    printf("\nThe sorted array: ");
+    for( i=0;i<n;i++)
+        printf("%d ",arr[i]);
+        
+    printf("\nNo.of comparisons: %d\n",comparisons);
+    printf("No.of swaps: %d\n",swaps);
 }
