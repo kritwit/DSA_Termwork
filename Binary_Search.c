f63d921c1537ff3e031105a371ac952990f6e0e1
Kritika Mittal <81286069+kritwit@users.noreply.github.com>
2023-01-23 15:43:59 +0530

    Update Binary_Search.c

--- Binary_Search.c
+++ Binary_Search.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+
 int binary_search(int arr[], int n, int key) {
     int left = 0;
     int right = n - 1;
@@ -12,4 +14,25 @@ int binary_search(int arr[], int n, int key) {
         }
     }
     return -1;
+}
+
+int main()
+{
+    int n,i,key,found;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter elements in array:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter element to be searched:");
+    scanf("%d",&key);
+    
+    found = binary_search(arr,n,key);
+    if(found == -1)
+        printf("Key not found");
+    else
+        printf("Key found at : %d",found+1);
 }
